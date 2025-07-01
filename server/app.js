085638d1d403dff7e4c0f8191506d57327982fff
goodness <anyanwugoodness83@yahoo.com>
2025-07-01 21:03:50 +0100

    update the frontend route

--- server/app.js
+++ server/app.js
@@ -16,7 +16,7 @@ const corsOptions = {
     'http://127.0.0.1:3001',
     'http://127.0.0.1:5173',
     'http://127.0.0.1:5174',
-    'https://artmagic-frontend.onrender.com/'
+    'https://artmagic-frontend.onrender.com'
   ],
   credentials: true,
   optionsSuccessStatus: 200,
